ca980918c2a625f7c775ab3326ea3ba8ef504ce2
Ezeh Andalene <andalenee@gmail.com>
2024-08-16 00:36:14 +0100

    Correction of MainContent

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
-function Maincontent() {
+function MainContent() {
     return ( <main>
         <p>I love to visit New York, Paris, and Tokyo.</p>
       </main> );
 }
 
-export default Maincontent;
+export default MainContent;
