510e7c24e5fdce0b1c03f286c598311a1d599de6
Ezeh Andalene <andalenee@gmail.com>
2024-08-19 00:45:58 +0100

    React adv project 2

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage.jsx'
 import UserProfile from './components/UserProfile.jsx'
 import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
+import Counter from './components/Counter.jsx'
 import Footer from './components/Footer.jsx'
 
 function App() {
@@ -40,6 +41,7 @@ function App() {
       
 
             <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+            <Counter />
 <Footer />
     </>
   )
