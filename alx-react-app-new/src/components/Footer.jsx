21b8ab9f3279b03047f75937c0155ae05f18ff02
Ezeh Andalene <andalenee@gmail.com>
2024-08-19 00:13:21 +0100

    advanced react project 1

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return ( <footer style={{backgroundColor:"blueviolet",margin:"10"
+    }}>
+        <p>© 2023 City Lovers</p>
+      </footer> );
+}
+
+export default Footer;
